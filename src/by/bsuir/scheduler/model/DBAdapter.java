b75a15e99cacea177de36ca3dce6a0594bd7dbce
nikolaev vladis <nikolaev.vladis@gmail.com>
2012-05-01 02:06:35 +0300

    ????????? ??????????? ?????? ? DBHelper ??? ?????????? ?????? DBAdapter ??? ?? ??????? ????????? ?????? DBAdapter

--- src/by/bsuir/scheduler/model/DBAdapter.java
+++ src/by/bsuir/scheduler/model/DBAdapter.java
@@ -6,6 +6,8 @@ import java.util.GregorianCalendar;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.provider.BaseColumns;
+import by.bsuir.scheduler.DayPagerAdapter;
 import by.bsuir.scheduler.parser.Lesson;
 import by.bsuir.scheduler.parser.Pushable;
 
@@ -36,6 +38,7 @@ public class DBAdapter implements Pushable, Closeable{
 	 * @param day
 	 */
 	public Day getDay(GregorianCalendar day){
+		Cursor cursor = dbHelper.getDay(String.valueOf(day.get(GregorianCalendar.DAY_OF_WEEK)));
 		return new Day(day, this);
 	}
 	
@@ -54,27 +57,44 @@ public class DBAdapter implements Pushable, Closeable{
 		//return day.get(GregorianCalendar.DAY_OF_WEEK)!=GregorianCalendar.SUNDAY;
 	}
 	
-	public Pair getPair(GregorianCalendar date, int scheduleId){
+	public Pair getPair(GregorianCalendar date) { // убрал параметр int scheduleId, т.к. получаю его из курсора
 		Cursor cursor = dbHelper.getDay(String.valueOf(date.get(GregorianCalendar.DAY_OF_WEEK)));
-		//TODO определить
-		return (new Day(date, this).getPair(scheduleId));
+		//TODO добавить проверку на номер подгруппы и номер недели
+		int[] times = new int[] {
+				cursor.getInt(cursor.getColumnIndex(DBColumns.START_HOUR)),
+				cursor.getInt(cursor.getColumnIndex(DBColumns.START_MINUTES)),
+				cursor.getInt(cursor.getColumnIndex(DBColumns.END_HOUR)),
+				cursor.getInt(cursor.getColumnIndex(DBColumns.END_MINUTES))
+		};
+		int week = cursor.getInt(cursor.getColumnIndex(DBColumns.WEEK));
+		int subGroup = cursor.getInt(cursor.getColumnIndex(DBColumns.SUBGROUP));
+		String lesson = cursor.getString(cursor.getColumnIndex(DBColumns.VIEW_SUBJECT));
+		int type = cursor.getInt(cursor.getColumnIndex(DBColumns.VIEW_SUBJECT_TYPE));
+		String sType = DayPagerAdapter.daysOfWeek[type];
+		String room = cursor.getString(cursor.getColumnIndex(DBColumns.ROOM));
+		String teacher = cursor.getString(cursor.getColumnIndex(DBColumns.VIEW_TEACHER));
+		int schedule = cursor.getInt(cursor.getColumnIndex(BaseColumns._ID));
+		String note = dbHelper.getNote(schedule, date);
+		Pair pair = new Pair(new Day(date, this), week, subGroup, lesson, type, sType, room, teacher, times, note, schedule); // не уверен по поводу правильности параметра container
+		return pair; //(new Day(date, this).getPair(scheduleId));
 	}
 	
-	public void changeNote(GregorianCalendar day, int schId, String note){
-		//TODO реализовать
+	public void changeNote(GregorianCalendar day, int scheduleId, String note){
+		dbHelper.updateNote(scheduleId, note, day);
 	}
 	
 	@Override
 	/**
 	 * Наш адаптер также будет принимать пары от парсера. В этом ему поможет наш метод
 	 */
-	public void push(Lesson lesson) {
+	public void push(Lesson lesson) { // для реализации метода необходимо наличие get/set у экземпляров класса Lesson
 		//TODO Определить
 	}
 
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
+		dbHelper.close();
 	}
 
 }
