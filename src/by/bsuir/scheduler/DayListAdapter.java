3da1f3e2e78d4711fb8fa08079130671fb100e22
svvok23 <svvok23@localhost>
2012-05-04 02:43:49 +0300

    ????? ????????? ??????????

--- src/by/bsuir/scheduler/DayListAdapter.java
+++ src/by/bsuir/scheduler/DayListAdapter.java
@@ -26,7 +26,8 @@ public class DayListAdapter extends BaseAdapter {
 	private List<Pair> mPairs;
 	private GregorianCalendar currentDay;
 
-	public DayListAdapter(Context context, GregorianCalendar day, List<Pair> pairs) {
+	public DayListAdapter(Context context, GregorianCalendar day,
+			List<Pair> pairs) {
 		inflater = LayoutInflater.from(context);
 		mPairs = pairs;
 		currentDay = day;
@@ -94,31 +95,32 @@ public class DayListAdapter extends BaseAdapter {
 		int cDay = currentDay.get(GregorianCalendar.DAY_OF_MONTH);
 		int cMomth = currentDay.get(GregorianCalendar.MONTH);
 		int green = Color.rgb(0, 178, 0);
-		
-		if (nDay == cDay && nMonth == cMomth) {
-		if (nHour > times[0] || (nHour == times[0] && nMinute > times[1])) {
-			if (nHour < times[2] || (nHour == times[2] && nMinute < times[3])) {
-				double pct = (double) (60 * (nHour - times[0]) + (nMinute - times[1])) / 90;
-				double sR = Color.red(Color.GRAY);
-				double sG = Color.green(Color.GRAY);
-				double sB = Color.blue(Color.GRAY);
-				double gR = Color.red(green) - sR;
-				double gG = Color.green(green) - sG;
-				double gB = Color.blue(green) - sB;
-				Log.i("DayListAdapter", "" + pct);
-				Log.i("DayListAdapter", "" + (int) (sR + gR * pct) + " "
-						+ (int) (sG + gG * pct) + " " + (int) (sB + gB * pct));
-				holder.statusBar.setBackgroundColor(Color.rgb((int) (sR + gR
-						* pct), (int) (sG + gG * pct), (int) (sB + gB * pct)));
-			} else {
-				holder.statusBar.setBackgroundColor(green);
-			}
-		} else {
-			holder.statusBar.setBackgroundColor(Color.GRAY);
-		}
-		}
 
-		holder.statusBar.setBackgroundColor(green);
+		if (nDay == cDay && nMonth == cMomth) {
+			if (nHour > times[0] || (nHour == times[0] && nMinute > times[1])) {
+				if (nHour < times[2]
+						|| (nHour == times[2] && nMinute < times[3])) {
+					double pct = (double) (60 * (nHour - times[0]) + (nMinute - times[1])) / 90;
+					double sR = Color.red(Color.GRAY);
+					double sG = Color.green(Color.GRAY);
+					double sB = Color.blue(Color.GRAY);
+					double gR = Color.red(green) - sR;
+					double gG = Color.green(green) - sG;
+					double gB = Color.blue(green) - sB;
+					Log.i("DayListAdapter", "" + pct);
+					Log.i("DayListAdapter", "" + (int) (sR + gR * pct) + " "
+							+ (int) (sG + gG * pct) + " "
+							+ (int) (sB + gB * pct));
+					holder.statusBar.setBackgroundColor(Color.rgb(
+							(int) (sR + gR * pct), (int) (sG + gG * pct),
+							(int) (sB + gB * pct)));
+				} else {
+					holder.statusBar.setBackgroundColor(green);
+				}
+			} else {
+				holder.statusBar.setBackgroundColor(Color.GRAY);
+			}
+		}
 
 		switch (lesson.getType()) {
 		case 1:
