f9a4999ba91368f1db5c2a1380ec01699edc6774
svvok23 <svvok23@localhost>
2012-05-04 16:00:55 +0300

    ????? ?????? ????????? ??????

--- src/by/bsuir/scheduler/activity/AlarmActivity.java
+++ src/by/bsuir/scheduler/activity/AlarmActivity.java
@@ -36,7 +36,7 @@ public class AlarmActivity extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.alarm);
 		sharedPref = getSharedPreferences(ALARM_PREF, MODE_PRIVATE);
-		
+
 		alarmType = (ListPreference) findPreference(ALARM_TYPE);
 		alarmBeforeLesson = (ListPreference) findPreference(ALARM_LESSON);
 		alarmTime = findPreference(ALARM_TIME);
@@ -44,34 +44,61 @@ public class AlarmActivity extends PreferenceActivity {
 		alarmVolume = findPreference(ALARM_VOLUME);
 		alarmVibration = (CheckBoxPreference) findPreference(ALARM_VIBRATION);
 
-		final String[] alarmTypes = getResources().getStringArray(R.array.alarm_types);
-		if (sharedPref.contains(ALARM_TYPE)) {
-			int indexOfType = sharedPref.getInt(ALARM_TYPE, 0);
-			alarmType.setSummary(alarmTypes[indexOfType]);
-		}
-		alarmBeforeLesson.setEnabled(Integer.parseInt(alarmType.getValue()) == 1);
-		alarmType.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+		final String[] alarmTypes = getResources().getStringArray(
+				R.array.alarm_types);
+		int indexOfType = sharedPref.getInt(ALARM_TYPE, 0);
+		alarmType.setSummary(alarmTypes[indexOfType]);
+		alarmBeforeLesson
+				.setEnabled(Integer.parseInt(alarmType.getValue()) == 1);
+		alarmType
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 					@Override
 					public boolean onPreferenceChange(Preference preference,
 							Object newValue) {
 						int value = Integer.parseInt((String) newValue);
 						alarmType.setValue((String) newValue);
 						alarmType.setSummary(alarmTypes[value]);
-						
+
 						alarmBeforeLesson.setEnabled(value == 1);
 						return false;
 					}
 				});
-		
+
+		// ЗАГЛУШКА
+		int numberOfLessons = 3;
+		//
+		String[] summaryValues = getResources().getStringArray(
+				R.array.alarm_summary_values);
+		final CharSequence[] entries = new String[numberOfLessons];
+		CharSequence[] entryValues = new String[numberOfLessons];
+		for (int i = 0; i < numberOfLessons; i++) {
+			entries[i] = summaryValues[i];
+			entryValues[i] = "" + i;
+		}
+		alarmBeforeLesson.setEntries(entries);
+		alarmBeforeLesson.setEntryValues(entryValues);
+		int indexOfLesson = sharedPref.getInt(ALARM_LESSON, 0);
+		alarmBeforeLesson.setSummary(summaryValues[indexOfLesson]);
+		alarmBeforeLesson
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						int value = Integer.parseInt((String) newValue);
+						alarmBeforeLesson.setValue((String) newValue);
+						alarmBeforeLesson.setSummary(entries[value]);
+						return false;
+					}
+				});
 	}
-	
+
 	@Override
 	protected void onStop() {
 		super.onStop();
-		
-		//длждлж
 		Editor editor = sharedPref.edit();
 		editor.putInt(ALARM_TYPE, Integer.parseInt(alarmType.getValue()));
+		editor.putInt(ALARM_LESSON,
+				Integer.parseInt(alarmBeforeLesson.getValue()));
 		editor.commit();
 	}
 
