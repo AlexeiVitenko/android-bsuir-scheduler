dae35d22de70c6c9d2120f2efa4131cdf1640983
svvok23 <svvok23@localhost>
2012-05-06 22:59:04 +0300

    ????????? ??? ?????? ????

--- src/by/bsuir/scheduler/activity/SettingsActivity.java
+++ src/by/bsuir/scheduler/activity/SettingsActivity.java
@@ -20,112 +20,148 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.DatePicker;
 
 public class SettingsActivity extends PreferenceActivity {
-	private static final int DATE_DIALOG=1001;
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-	
+	private static final int DATE_DIALOG = 1001;
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
+			"dd.MM.yyyy");
+
 	private Preference mSemesterLength;
 	private Preference mStartDate;
 	private EditTextPreference mGroupNumber;
 	private ListPreference mSubGroup;
 	private SharedPreferences mPref;
 	private DBAdapter mAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		
+
 		mAdapter = DBAdapter.getInstance(getApplicationContext());
 		mPref = PreferenceManager.getDefaultSharedPreferences(this);
-		
+
 		mSemesterLength = findPreference(getString(R.string.semester_length_weeks));
-		mSemesterLength.setSummary(""+mPref.getString(getString(R.string.semester_length_weeks),""+(-1))+getString(R.string.weeks));
-		mSemesterLength.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				mSemesterLength.setSummary(""+(String)newValue+getString(R.string.weeks));
-				mAdapter.recalculateSomeThings();
-				return true;
-			}
-		});
-		
+		mSemesterLength.setSummary(""
+				+ mPref.getString(getString(R.string.semester_length_weeks), ""
+						+ (-1)) + getString(R.string.weeks));
+		mSemesterLength
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						mSemesterLength.setSummary("" + (String) newValue
+								+ getString(R.string.weeks));
+						mAdapter.recalculateSomeThings();
+						return true;
+					}
+				});
+
 		mStartDate = findPreference(getString(R.string.semester_start_day));
-		mStartDate.setSummary(formatDate(mPref.getLong(getString(R.string.semester_start_day), System.currentTimeMillis())));
-		mStartDate.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-			
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				showDialog(DATE_DIALOG);
-				return false;
-			}
-		});
-		
+		mStartDate.setSummary(formatDate(mPref.getLong(
+				getString(R.string.semester_start_day), getDefStartDay())));
+		mStartDate
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						showDialog(DATE_DIALOG);
+						return false;
+					}
+				});
+
 		mGroupNumber = (EditTextPreference) findPreference(getString(R.string.group_number));
 		mGroupNumber.setSummary(mGroupNumber.getText());
-		mGroupNumber.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				preference.setSummary((String)newValue);
-				return true;
-			}
-		});
-		
-		mSubGroup = (ListPreference)findPreference(getString(R.string.preference_sub_group_list));
+		mGroupNumber
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference.setSummary((String) newValue);
+						return true;
+					}
+				});
+
+		mSubGroup = (ListPreference) findPreference(getString(R.string.preference_sub_group_list));
 		mSubGroup.setSummary(mSubGroup.getEntry());
-		mSubGroup.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				mSubGroup.setSummary(getResources().getStringArray(R.array.preferences_sub_group_entries)[Integer.parseInt((String)newValue)]);
-				return true;
-			}
-		});
+		mSubGroup
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						mSubGroup.setSummary(getResources().getStringArray(
+								R.array.preferences_sub_group_entries)[Integer
+								.parseInt((String) newValue)]);
+						return true;
+					}
+				});
 	}
-	
+
+	private long getDefStartDay() {
+		// пару дефолтов. думаю правильнее будет для приложения. 
+		// если не согласен - обсудим с Денисом, как поступают обычно.
+		GregorianCalendar today = new GregorianCalendar(Locale.getDefault());
+		int year = today.get(GregorianCalendar.YEAR);
+		long defFirstSem = new GregorianCalendar(year,
+				GregorianCalendar.SEPTEMBER, 1).getTimeInMillis();
+		long defSecondSem = new GregorianCalendar(year,
+				GregorianCalendar.FEBRUARY, 6).getTimeInMillis();
+		long defValue = today.getTimeInMillis();
+		if (defValue < defFirstSem) {
+			defValue = defSecondSem;
+		} else {
+			defValue = defFirstSem;
+		}
+		//
+		return defValue;
+	}
+
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
 		case DATE_DIALOG:
 			GregorianCalendar gc = new GregorianCalendar(Locale.getDefault());
-			gc.setTimeInMillis(mPref.getLong(getString(R.string.semester_start_day), System.currentTimeMillis()));
-			DatePickerDialog dpd = new DatePickerDialog(this, new OnDateSetListener() {
-				
-				@Override
-				public void onDateSet(DatePicker view, int year, int monthOfYear,
-						int dayOfMonth) {
-					GregorianCalendar gcc = new GregorianCalendar(year, monthOfYear, dayOfMonth);
-					mStartDate.setSummary(formatDate(gcc.getTimeInMillis()));
-					Editor edit = mPref.edit();
-					edit.putLong(getString(R.string.semester_start_day), gcc.getTimeInMillis());
-					edit.commit();
-					mAdapter.recalculateSomeThings();
-				}
-			}, gc.get(GregorianCalendar.YEAR), gc.get(GregorianCalendar.MONTH), gc.get(GregorianCalendar.DAY_OF_MONTH));
+			gc.setTimeInMillis(mPref.getLong(
+					getString(R.string.semester_start_day), getDefStartDay()));
+			DatePickerDialog dpd = new DatePickerDialog(this,
+					new OnDateSetListener() {
+
+						@Override
+						public void onDateSet(DatePicker view, int year,
+								int monthOfYear, int dayOfMonth) {
+							GregorianCalendar gcc = new GregorianCalendar(year,
+									monthOfYear, dayOfMonth);
+							mStartDate.setSummary(formatDate(gcc
+									.getTimeInMillis()));
+							Editor edit = mPref.edit();
+							edit.putLong(
+									getString(R.string.semester_start_day),
+									gcc.getTimeInMillis());
+							edit.commit();
+							mAdapter.recalculateSomeThings();
+						}
+					}, gc.get(GregorianCalendar.YEAR),
+					gc.get(GregorianCalendar.MONTH),
+					gc.get(GregorianCalendar.DAY_OF_MONTH));
 			return dpd;
 
 		default:
 			return super.onCreateDialog(id);
 		}
 	}
+
 	/*
-	@Override
-	protected void onPrepareDialog(int id, Dialog dialog) {
-		switch (id) {
-		case DATE_DIALOG:
-			((DatePickerDialog)dialog).set
-			break;
-		default:
-			super.onPrepareDialog(id, dialog);
-			break;
-		}
-	}
-	*/
-	private static String formatDate(long date ){
+	 * @Override protected void onPrepareDialog(int id, Dialog dialog) { switch
+	 * (id) { case DATE_DIALOG: ((DatePickerDialog)dialog).set break; default:
+	 * super.onPrepareDialog(id, dialog); break; } }
+	 */
+	private static String formatDate(long date) {
 		return dateFormat.format(new Date(date));
 	}
 }
