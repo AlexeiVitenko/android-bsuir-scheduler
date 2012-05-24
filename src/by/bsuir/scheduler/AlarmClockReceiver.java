ef4f44748412ad4bd2ad1c5740543d14646daa5c
AlexeiVitenko <AlexeiVitenko@localhost>
2012-05-25 01:22:44 +0300

    ??????????? ??????????

--- src/by/bsuir/scheduler/AlarmClockReceiver.java
+++ src/by/bsuir/scheduler/AlarmClockReceiver.java
@@ -14,6 +14,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -28,7 +29,7 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 	public static final String ALARM_TIME = "alarm_time_блеать";
 	public static final String ALARM_LESSON_ID = "lesson_id";
 
-	private Context context;
+	private Context mContext;
 	private DBAdapter dbAdapter;
 	private SharedPreferences alarmPref;
 	private Day day;
@@ -36,26 +37,26 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		this.context = context;
+		this.mContext = context;
 		alarmPref = PreferenceManager.getDefaultSharedPreferences(context);
 		
 		if (alarmPref.getBoolean(AlarmActivity.ALARM_CLOCK, false)) {
 			Log.i("AlarmClockReceiver",
 					"intent " + intent.getStringExtra(ALARM_STATUS));
-
+			
 			// если вызывается будильник
 			//FIXME тут ругается на nullpointer
 			if(intent.getExtras()!=null && intent.getExtras().containsKey(ALARM_STATUS)){
 				if (intent.getStringExtra(ALARM_STATUS).equals(CLOCK)) {
 					PowerManager.WakeLock wl;
-					PowerManager pm = (PowerManager) context.getSystemService(context.POWER_SERVICE);
+					PowerManager pm = (PowerManager) mContext.getSystemService(context.POWER_SERVICE);
 					wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK|PowerManager.ACQUIRE_CAUSES_WAKEUP, "My Tag");
 					wl.acquire();
 					Log.d("wake","up");
-					Intent it = new Intent(context, AlarmClockActivity.class);
+					Intent it = new Intent(mContext, AlarmClockActivity.class);
 					it.putExtras(intent.getExtras());
 					it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-					context.startActivity(it);
+					mContext.startActivity(it);
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -65,7 +66,7 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 					wl.release();
 				} else {
 					if (intent.getStringExtra(ALARM_STATUS).equals(CHANGE)) {
-						clearIntents(context);
+						clearIntents(mContext);
 					}
 				}
 			}
@@ -74,16 +75,18 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 			setNewAlarmClock();
 
 		} else {
-			clearIntents(context);
+			clearIntents(mContext);
 		}
 	}
 
 	private void init() {
-		dbAdapter = DBAdapter.getInstance(context.getApplicationContext());
+		dbAdapter = DBAdapter.getInstance(mContext.getApplicationContext());
 		time = new GregorianCalendar(Locale.getDefault());
 		day = dbAdapter.getDay(time);
-		time.setTimeInMillis(AlarmActivity.getAlarmTimeLong(context, day));
-
+		time.setTimeInMillis(AlarmActivity.getAlarmTimeLong(mContext, day));
+		Editor editor = PreferenceManager.getDefaultSharedPreferences(mContext).edit();
+		editor.putLong(mContext.getString(R.string.key_get_next_pairs_and_alarm), time.getTimeInMillis());
+		editor.commit();
 		GregorianCalendar now = new GregorianCalendar(Locale.getDefault());
 		boolean isChange = false;
 		// поиск ближайшего будильника
@@ -95,14 +98,14 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 
 		if (isChange) {
 			day = dbAdapter.getDay(time);
-			time.setTimeInMillis(AlarmActivity.getAlarmTimeLong(context, day));
+			time.setTimeInMillis(AlarmActivity.getAlarmTimeLong(mContext, day));
 		}
 	}
 	
 	private void setNewAlarmClock() {
-		AlarmManager alarmManager = (AlarmManager) context
+		AlarmManager alarmManager = (AlarmManager) mContext
 				.getSystemService(Context.ALARM_SERVICE);
-		Intent intent = new Intent(context.getApplicationContext(),
+		Intent intent = new Intent(mContext.getApplicationContext(),
 				AlarmClockReceiver.class);
 		intent.putExtra("МАГИЯ", "НО БЕЗ ЭТОЙ ХЕРНИ НЕ РАБОТАЕТ?!");
 		intent.putExtra(ALARM_TIME, time.getTimeInMillis());
@@ -118,19 +121,19 @@ public class AlarmClockReceiver extends BroadcastReceiver {
 		}
 
 		PendingIntent pi = PendingIntent.getBroadcast(
-				context.getApplicationContext(), ALARM_ID, intent,
+				mContext.getApplicationContext(), ALARM_ID, intent,
 				PendingIntent.FLAG_UPDATE_CURRENT);
 		alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), pi);
 		Calendar at = Calendar.getInstance();
 		String day = "";
 		if (at.get(Calendar.DAY_OF_YEAR)!=time.get(Calendar.DAY_OF_YEAR)) {
-			day = " " + context.getResources().getStringArray(R.array.days_of_week_abb)[time.get(Calendar.DAY_OF_WEEK)-1];
+			day = " " + mContext.getResources().getStringArray(R.array.days_of_week_abb)[time.get(Calendar.DAY_OF_WEEK)-1];
 		}
 		Toast.makeText(
-				context,
-				context.getResources().getString(R.string.app_name)
+				mContext,
+				mContext.getResources().getString(R.string.app_name)
 						+ ". "
-						+ context.getResources()
+						+ mContext.getResources()
 								.getString(R.string.alarm_toast) + " "
 						+ AlarmActivity.formatTime(time.getTimeInMillis())+day,
 				Toast.LENGTH_LONG).show();
